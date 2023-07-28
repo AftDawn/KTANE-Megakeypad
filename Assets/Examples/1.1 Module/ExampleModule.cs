1d7db895099d00bae9c3be09464761ced1397ead
AftDawn <declanjeffrey6@gmail.com>
2023-07-28 21:20:14 +1000

    fixed things (hopefully)

---
+++ Assets/Examples/1.1 Module/ExampleModule.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ExampleModule : MonoBehaviour
+{
+    public KMSelectable[] buttons;
+
+    int correctIndex;
+    bool isActivated = false;
+
+    public List<int> arduinoRGBValues = new List<int>() {255,0,0};
+
+    void Start()
+    {
+        Init();
+
+        GetComponent<KMBombModule>().OnActivate += ActivateModule;
+    }
+
+    void Init()
+    {
+        correctIndex = Random.Range(0, 4);
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            string label = i == correctIndex ? "O" : "X";
+
+            TextMesh buttonText = buttons[i].GetComponentInChildren<TextMesh>();
+            buttonText.text = label;
+            int j = i;
+            buttons[i].OnInteract += delegate () { OnPress(j == correctIndex); return false; };
+        }
+    }
+
+    void ActivateModule()
+    {
+        isActivated = true;
+    }
+
+    void OnPress(bool correctButton)
+    {
+        GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        GetComponent<KMSelectable>().AddInteractionPunch();
+
+        if (!isActivated)
+        {
+            Debug.Log("Pressed button before module has been activated!");
+            GetComponent<KMBombModule>().HandleStrike();
+        }
+        else
+        {
+            Debug.Log("Pressed " + correctButton + " button");
+            if (correctButton)
+            {
+                GetComponent<KMBombModule>().HandlePass();
+            }
+            else
+            {
+                GetComponent<KMBombModule>().HandleStrike();
+            }
+        }
+    }
+}
---
+++ Assets/megakeypad.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using RNG = UnityEngine.Random;
+using KModkit;
+
+public class megakeypad : MonoBehaviour
+{
+	new public KMAudio audio;
+	public KMBombInfo bomb;
+
+	public KMSelectable[] keypad;
+	public Material[] keypadLedMats;
+
+	string[,] keypadSolvingArray = new string[ , ] { 
+		{  "1",  "2",  "3" }, {  "4",  "5",  "6" }, {  "7",  "8",  "9" },
+		{ "10", "11", "12" }, { "13", "14", "15" }, { "16", "17", "18" },
+		{ "19", "20", "21" }, { "22", "23", "24" }, { "25",  "5", "26" },
+		{ "27", "22", "23" }, { "28",  "6", "29" }, { "30",  "4", "31" },
+		{ "14", "27", "32" }, {  "5", "17", "12" }, {  "2", "19", "33" },
+		{ "26", "24", "34" }, { "35", "36",  "5" }, { "11", "25", "36" },
+		{ "28", "29",  "7" }, { "15", "16", "37" }, { "35", "38", "30" },
+		{  "9",  "3", "20" }, {  "1",  "8", "33" }, {  "2", "18", "21" }
+	};
+
+	static int moduleIdCounter = 1;
+	int moduleId = 0;
+	private bool moduleSolved = false;
+
+
+
+	void Awake ()
+	{
+		moduleId = moduleIdCounter++;
+
+		foreach (KMSelectable key in keypad)
+		{
+			KMSelectable pressedkey = key;
+			key.OnInteract += delegate () { HandleKey(pressedkey); return false; };
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.LogFormat("[Megakeypad #{0}] Hello World!", moduleId);
+		Determine2x3KeyGrid("TL");
+	}
+
+	void Determine2x3KeyGrid(string quadrent)
+	{
+		return;
+	}
+
+	void AvoidCollisions()
+	{
+
+	}
+
+	Vector2 GetCoordsFromKeyName(KMSelectable kp)
+	{
+		string kpname = kp.name;
+		string[] kpcoords = kpname.Split('_');
+		return new Vector2(int.Parse(kpcoords[1]), int.Parse(kpcoords[2]));
+	}
+
+	void HandleKey(KMSelectable key)
+	{
+
+		//fix name into something alot more human readable, like a coordinet system!
+		
+		Vector2 keycoords = GetCoordsFromKeyName(key);
+
+		Debug.LogFormat("[Megakeypad #{0}] Pressed key at {1}/{2}.", moduleId, keycoords.x, keycoords.y);
+	}
+}
