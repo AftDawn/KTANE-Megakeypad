57b9e501dbdd427d0c67d40c3821116a1c563660
AftDawn <declanjeffrey6@gmail.com>
2023-07-29 15:58:44 +1000

    changed folder for manual and did alot with it

--- Assets/megakeypad.cs
+++ Assets/megakeypad.cs
@@ -14,16 +14,18 @@ public class megakeypad : MonoBehaviour
 	public KMSelectable[] keypad;
 	public Material[] keypadLedMats;
 
-	string[,] keypadSolvingArray = new string[ , ] { 
-		{  "1",  "2",  "3" }, {  "4",  "5",  "6" }, {  "7",  "8",  "9" },
-		{ "10", "11", "12" }, { "13", "14", "15" }, { "16", "17", "18" },
-		{ "19", "20", "21" }, { "22", "23", "24" }, { "25",  "5", "26" },
-		{ "27", "22", "23" }, { "28",  "6", "29" }, { "30",  "4", "31" },
-		{ "14", "27", "32" }, {  "5", "17", "12" }, {  "2", "19", "33" },
-		{ "26", "24", "34" }, { "35", "36",  "5" }, { "11", "25", "36" },
-		{ "28", "29",  "7" }, { "15", "16", "37" }, { "35", "38", "30" },
-		{  "9",  "3", "20" }, {  "1",  "8", "33" }, {  "2", "18", "21" }
+	int[,] keypadSolvingArray = new int[ , ] { 
+		{  1,  2,  3 }, {  4,  5,  6 }, {  7,  8,  9 },
+		{ 10, 11, 12 }, { 13, 14, 15 }, { 16, 17, 18 },
+		{ 19, 20, 21 }, { 22, 23, 24 }, { 25,  5, 26 },
+		{ 27, 22, 23 }, { 28,  6, 29 }, { 30,  4, 31 },
+		{ 14, 27, 32 }, {  5, 17, 12 }, {  2, 19, 33 },
+		{ 26, 24, 34 }, { 35, 36,  5 }, { 11, 25, 36 },
+		{ 28, 29,  7 }, { 15, 16, 37 }, { 35, 38, 30 },
+		{  9,  3, 20 }, {  1,  8, 33 }, {  2, 18, 21 }
 	};
+
+	int[,] keypadAssignedSymbols = new int[6,4];
 
 	static string moduleName = "Megakeypad";
 
@@ -31,7 +33,7 @@ public class megakeypad : MonoBehaviour
 	int moduleId = 0;
 	private bool moduleSolved = false;
 
-
+	//Logger stuff
 	void Logger(string logMessage)
 	{
 		Debug.LogFormat("[{0} #{1}] {2}", moduleName, moduleId, logMessage);
@@ -65,12 +67,22 @@ public class megakeypad : MonoBehaviour
 		LoggerTitleAndInfo();
 	}
 
-	void Determine2x3KeyGrid()
+	void PickSymbols()
 	{
 
 	}
 
 	void AvoidSymbolCollisions()
+	{
+
+	}
+
+	void Complete()
+	{
+
+	}
+
+	void Strike()
 	{
 
 	}
